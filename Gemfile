8d18d8b2ea8b39981756ec276342498528ab0c50
Matthias Vitte <matthias@zenchef.com>
2017-01-10 18:34:10 +0100

    adding bootrsap

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
   gem "binding_of_caller"
+  gem 'callr', '~> 1.0'
+  gem 'bootstrap', '~> 4.0.0.alpha6'
+  gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 end
 
 group :development do
@@ -44,6 +47,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/controllers/pushings_controller.rb
+++ app/controllers/pushings_controller.rb
@@ -1,12 +1,51 @@
 class PushingsController < ApplicationController
 
+
+
   def home
 
   end
 
   def send_sms
-    phone = params["phone"].gsub!(/[" "]/, '')
+    require 'callr'
+    api = CALLR::Api.new('zenchef', '59Ng8POwYE')
+    phone = params["phone"].gsub(/[" "]/, '')
+    add_region = phone.first.gsub(/["0"]/, '+33') + phone[1..11]
 
-raise
+    step_of_reconvering = params['step'].to_i
+
+    case step_of_reconvering
+    when 1
+      if phone.first == "+"
+        api.call('sms.send', 'Zenchef', "#{phone}", "Bonjour, j'ai tenté de vous joindre concernant votre facturation. Pourriez-vous me recontacter au 0176440040? Je vous remercie d'avance. Matthias Vitte", nil)
+      else
+        api.call('sms.send', 'Zenchef', "#{add_region}", "Bonjour, j'ai tenté de vous joindre concernant votre facturation. Pourriez-vous me recontacter au 0176440040? Je vous remercie d'avance. Matthias Vitte", nil)
+      end
+      @result = "#{params['name']} #{params['family']} a bien reçu le sms de l'étape #{step_of_reconvering}"
+    when 2
+      if phone.first == "+"
+        api.call('sms.send', 'Zenchef', "#{phone}", "Bonjour, certaines de vos factures n'ont pas été réglées. Afin d'eviter la suspension de vos services, merci de me contacter au 0176440040. Matthias Vitte", nil)
+      else
+        api.call('sms.send', 'Zenchef', "#{add_region}", "Bonjour, certaines de vos factures n'ont pas été réglées. Afin d'eviter la suspension de vos services, merci de me contacter au 0176440040. Matthias Vitte", nil)
+      end
+      @result = "#{params['name']} #{params['family']} a bien reçu le sms de l'étape #{step_of_reconvering}"
+    when 3
+      if phone.first == "+"
+       api.call('sms.send', 'Zenchef', "#{phone}", "Bonjour, afin d'eviter la suspension de vos services et une majoration de 30%, merci de régler vos factures dans les 8 jours. Matthias Vitte 0176440040", nil)
+      else
+        api.call('sms.send', 'Zenchef', "#{add_region}", "Bonjour, afin d'eviter la suspension de vos services et une majoration de 30%, merci de régler vos factures dans les 8 jours. Matthias Vitte 0176440040", nil)
+      end
+       @result = "#{params['name']} #{params['family']} a bien reçu le sms de l'étape #{step_of_reconvering}"
+    when 4
+      if phone.first == "+"
+        api.call('sms.send', 'Zenchef', "#{phone}", "Bonjour, vos factures demeurent impayées. Ces dernières seront donc recouvrées avec une majoration de 30%, votre service est désormais suspendu. Matthias Vitte", nil)
+      else
+        api.call('sms.send', 'Zenchef', "#{add_region}", "Bonjour, vos factures demeurent impayées. Ces dernières seront donc recouvrées avec une majoration de 30%, votre service est désormais suspendu. Matthias Vitte", nil)
+      end
+      @result = "#{params['name']} #{params['family']} a bien reçu le sms de l'étape #{step_of_reconvering}"
+    end
+
+    redirect_to :root
+
   end
 end
